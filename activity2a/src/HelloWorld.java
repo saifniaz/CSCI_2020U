3a3ae5c4fc52fc36468b16e0969a602736297361
Saif Niaz <saifniaz.3@gmail.com>
2016-01-21 10:31:00 -0500

    We went full on Quantum2

--- activity2a/src/HelloWorld.java
+++ activity2a/src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.printf("Hello Canada\n");
+		System.out.printf("Hello from the other side \n");
 	}
+}
+
+public void doNothing(){
+	System.out.println("Nothing");
 }
